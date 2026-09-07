178c3f8c3bd222bb7e47a15d052378d9aab492af
madematics <mmade.mths@gmail.com>
2026-09-07 05:13:25 +0300

    Add event listener for details button alert

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const detailsButton = document.querySelector(".details-button");
 
+detailsButton.addEventListener("click", function () {
+    alert(
+        "Euler's Challenge — 1st Place\n\n" +
+        "2025 Summer Edition — Inaugural Edition\n\n" +
+        "1st Place out of 45 participants\n" +
+        "Score: 18/35\n" +
+        "Average Score: 2/35\n" +
+        "Median Score: 0/35"
+    );
+});
